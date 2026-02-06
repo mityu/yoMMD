4b8cb5d75e01a67ba693162623faf8c1508c9fd2
mityu <mityu.mail@gmail.com>
2026-02-07 02:26:07 +0900

    feat: Initial implementation of sokol-app backend

--- flake.nix
+++ flake.nix
@@ -1,34 +1,54 @@
 {
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+    flake-parts.url = "github:hercules-ci/flake-parts";
   };
 
   outputs =
-    { nixpkgs, ... }:
-    let
-      forEachDarwinSystem = nixpkgs.lib.genAttrs [
+    inputs@{ flake-parts, ... }:
+    flake-parts.lib.mkFlake { inherit inputs; } {
+      systems = [
+        "x86_64-linux"
+        "aarch64-linux"
         "aarch64-darwin"
         "x86_64-darwin"
       ];
-    in
-    {
-      devShells = forEachDarwinSystem (
-        system:
+      perSystem =
+        {
+          pkgs,
+          lib,
+          system,
+          ...
+        }:
         let
-          pkgs = import nixpkgs { inherit system; };
+          isLinux = lib.strings.hasSuffix "linux" system;
         in
         {
-          default = pkgs.mkShell {
-            nativeBuildInputs = with pkgs; [
-              libcxx
-              libllvm
-              clang
-              gnumake
-              cmake
-              ninja
-            ];
+          devShells.default = pkgs.mkShell {
+            nativeBuildInputs =
+              with pkgs;
+              [
+                gnumake
+                cmake
+                ninja
+                libcxx
+                libllvm
+                clang
+              ]
+              ++ lib.optionals isLinux [
+                (lib.lowPrio xorg.xorgproto)
+                libxcb.dev
+                libxrandr
+                libxinerama
+                libx11
+                libxcursor
+                libxxf86vm
+                libGL
+                glfw
+
+                libxi
+              ];
           };
-        }
-      );
+        };
     };
 }
--- libs.mm
+++ libs.mm
@@ -6,10 +6,18 @@
 #define SOKOL_METAL
 #elif defined(PLATFORM_WINDOWS)
 #define SOKOL_D3D11
+#elif defined(PLATFORM_LINUX)
+#define SOKOL_GLCORE
+#define BACKEND_SOKOL_APP
 #endif
 
 #include "sokol_gfx.h"
 #include "sokol_time.h"
+
+#ifdef BACKEND_SOKOL_APP
+#include "sokol_app.h"
+#include "sokol_glue.h"  // sokol_glue.h must be included after sokol_gfx.h and sokol_app.h
+#endif
 
 #define STB_IMAGE_IMPLEMENTATION
 #include "stb_image.h"
